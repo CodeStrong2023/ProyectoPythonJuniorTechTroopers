b7d86712a940890e34d8a136647ce293d2ecc69e
Joaquín Viñolo <joaquin9811@gmail.com>
2024-06-20 18:37:56 -0300

    Insertinfo actualizado

--- src/database/InsertInfo.py
+++ src/database/InsertInfo.py
@@ -4,35 +4,36 @@ import mysql.connector
 from mysql.connector import Error
 from src.database.Connection import Connection
 
+
 class InsertInfo:
     """
-        Clase para insertar información de usuario en la base de datos.
+    Clase para insertar información de usuario en la base de datos.
 
-        Attributes:
-            conexion: Conexión a la base de datos.
-        """
+    Attributes:
+        conexion: Conexión a la base de datos.
+    """
     def __init__(self, db_key='1'):
         """
-                Constructor de la clase.
+        Constructor de la clase.
 
-                Args:
-                    db_key (str): Clave de la base de datos.
-                """
+        Args:
+            db_key (str): Clave de la base de datos.
+        """
         self.conexion = Connection(db_key).connect()
 
     def insertar_usuario(self, datos_usuario):
         """
-                Inserta los datos de un usuario en la base de datos.
+        Inserta los datos de un usuario en la base de datos.
 
-                Args:
-                    datos_usuario (dict): Datos del usuario a insertar.
+        Args:
+            datos_usuario (dict): Datos del usuario a insertar.
 
-                Raises:
-                    Error: Si ocurre un error durante la inserción de datos.
-                """
+        Raises:
+            pymysql.Error: Si ocurre un error durante la inserción de datos.
+        """
         sql = """
-        INSERT INTO Usuarios (username, password, nombre, apellido, documento, fecha_nacimiento, edad, telefono, saldo, activo)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        INSERT INTO Usuarios (username, password, nombre, apellido, email, documento, fecha_nacimiento, edad, telefono, saldo, activo)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
         try:
             cursor = self.conexion.cursor()
@@ -41,6 +42,7 @@ class InsertInfo:
                 datos_usuario['password'],
                 datos_usuario['nombre'],
                 datos_usuario['apellido'],
+                datos_usuario['email'],
                 datos_usuario['documento'],
                 datos_usuario['fecha_nacimiento'],
                 datos_usuario['edad'],
@@ -53,3 +55,4 @@ class InsertInfo:
         except Error as e:
             print(f"Error al insertar usuario: {e}")
             self.conexion.rollback()
+
